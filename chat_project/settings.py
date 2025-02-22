2d768dd3862b179cfa40c8d0ceacbb60a0b918eb
Himanshu <32831023+himanshusolanki1@users.noreply.github.com>
2025-02-22 13:06:27 +0530

    Update settings.py

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -139,3 +139,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'chat:index'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
+CSRF_TRUSTED_ORIGINS = ["https://django-procution-4ecc.up.railway.app"]
